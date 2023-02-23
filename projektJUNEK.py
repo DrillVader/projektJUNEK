2b03ecef27a9e4e7538af9eb428fe808475911c0
DrillVader <vojta.junek@tiscali.cz>
2023-02-23 19:24:08 +0100

    commitI_HOPE_FINAL

--- projektJUNEK.py
+++ projektJUNEK.py
@@ -30,24 +30,49 @@ while True:
     else:
         print("Incorrect username or password, please try again.")
 
-txt_soubor = open("task_template.txt", mode="r")
-obsah_txt = txt_soubor.read()
-textList = obsah_txt.split(";")
-txt_soubor.close()
+def txt():
+    text = "Situated about 10 miles west of Kemmerer,Fossil Butte is a ruggedly impressive topographic feature that rises sharply '\
+    some 1000 feet above Twin Creek Valley '\
+    to an elevation of more than 7500 feet '\
+    above sea level. The butte is located just '\
+    north of US 30N and the Union Pacific Railroad, '\
+    which traverse the valley.; '\
+    At the base of Fossil Butte are the bright '\
+    red, purple, yellow and gray beds of the Wasatch '\
+    Formation. Eroded portions of these horizontal '\
+    beds slope gradually upward from the valley floor '\
+    and steepen abruptly. Overlying them and extending '\
+    to the top of the butte are the much steeper '\
+    buff-to-white beds of the Green River Formation, '\
+    which are about 300 feet thick.;'\
+    The monument contains 8198 acres and protects'\
+    a portion of the largest deposit of freshwater fish'\
+    fossils in the world. The richest fossil fish deposits'\
+    are found in multiple limestone layers, which lie some'\
+    100 feet below the top of the butte. The fossils'\
+    represent several varieties of perch, as well as'\
+    other freshwater genera and herring similar to those'\
+    in modern oceans. Other fish such as paddlefish,'\
+    garpike and stingray are also present."
+    textList = text.split(";")    
+    return textList
+
+
+
 
 print("Zadejte prosím číslo 1-3 pro výběr textu, který chcete analyzovat: ")
 volba = input()
 
 
 def pocetslov():
-    res = len(re.findall(r'\w+', textList[int(volba)-1]))    
+    res = len(re.findall(r'\w+', txt()[int(volba)-1]))    
     print("pocet slov v textu je: ",str(res))
 
 pocetslov()
 
 def zacinajiVelkym():
     list1 = []
-    for word in textList[int(volba)-1].split():
+    for word in txt()[int(volba)-1].split():
         
         if word[0].isupper() and word[0].isalpha():
             list1.append(word)
@@ -59,7 +84,7 @@ zacinajiVelkym()
 
 def jsouVelkym():
     list1 = []
-    for word in textList[int(volba)-1].split():
+    for word in txt()[int(volba)-1].split():
         
         if word.isupper() and word[0].isalpha():
             list1.append(word)
@@ -77,7 +102,7 @@ jsouVelkym()
 
 def jsoumalym():
     list1 = []
-    pocet = sum(map(str.islower, textList[int(volba)-1].split()))
+    pocet = sum(map(str.islower, txt()[int(volba)-1].split()))
     print("pocet slov malým písmem je:",pocet)
     
 
@@ -85,7 +110,7 @@ jsoumalym()
 
 def cislice():
     list1 = []
-    for word in textList[int(volba)-1]:
+    for word in txt()[int(volba)-1]:
         for pismeno in word:
             if pismeno.isnumeric():
                 list1.append(pismeno)
@@ -96,7 +121,7 @@ cislice()
 
 def suma():
     vysledek = 0
-    for word in textList[int(volba)-1].split():
+    for word in txt()[int(volba)-1].split():
         if word.isdigit():
             vysledek += int(word)
         
@@ -106,14 +131,16 @@ suma()
 #za slovo se počíta s minimalně 2 znaky
 def graf():
     delky_slov = {}
-    for word in textList[int(volba) - 1].split():
+    for word in txt()[int(volba) - 1].split():
         if word.isalpha():
             if len(word) in delky_slov:
-                delky_slov[len(word)] +=1
+                delky_slov[len(word)] += 1
             else:
                 delky_slov[len(word)] = 1
-    word_lengths = dict(sorted(delky_slov.items()))
-    for length, count in delky_slov.items():
+                
+    word_lengths = dict(sorted(delky_slov.items())) # sort the dictionary by keys
+    
+    for length, count in word_lengths.items(): # iterate over the sorted dictionary
         print(f"{length:2d}|{'*'* count:<15} {count}")
     
     
